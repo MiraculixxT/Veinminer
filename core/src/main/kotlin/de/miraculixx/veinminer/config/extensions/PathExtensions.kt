6096c4f46d1e284dd600cde1d240f7956c9b308a
Miraculixx <miraculixxyt@gmail.com>
2025-03-27 16:04:19 +0100

    Encode default configs

--- core/src/main/kotlin/de/miraculixx/veinminer/config/extensions/PathExtensions.kt
+++ core/src/main/kotlin/de/miraculixx/veinminer/config/extensions/PathExtensions.kt
@@ -1,6 +1,7 @@
 package de.miraculixx.veinminer.config.extensions
 
 import de.miraculixx.veinminer.config.utils.json
+import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import net.kyori.adventure.text.format.TextColor
 import java.nio.file.Path
@@ -8,11 +9,14 @@ import kotlin.io.path.createFile
 import kotlin.io.path.createParentDirectories
 import kotlin.io.path.exists
 import kotlin.io.path.readText
+import kotlin.io.path.writeText
 
 inline fun <reified T> Path.load(default: T, instance: Json = json): T {
     return if (!exists()) {
         createParentDirectories()
         createFile()
+        writeText(instance.encodeToString(default))
+        println("[Veinminer] Created ${this.fileName} default config")
         default
     } else {
         try {
