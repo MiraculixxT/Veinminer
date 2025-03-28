33af27b012c3f22c0588671fb1993c14e015e069
Miraculixx <miraculixxyt@gmail.com>
2025-03-28 20:13:23 +0100

    Final fixes (why is this namedElements thing so weird)

--- veinminer-client/build.gradle.kts
+++ veinminer-client/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 dependencies {
-    implementation(include(project(":core", configuration = "namedElements"))!!)
+    implementation(include(project(":core"))!!)
     implementation(project(":veinminer-fabric", configuration = "namedElements"))
 }
 
