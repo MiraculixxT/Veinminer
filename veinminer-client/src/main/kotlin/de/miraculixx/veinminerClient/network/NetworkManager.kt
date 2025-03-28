61504e725306f768cfc1bab7dce61aa35a17e9c2
Miraculixx <miraculixxyt@gmail.com>
2025-03-28 18:11:59 +0100

    Improve networking in single player

--- veinminer-client/src/main/kotlin/de/miraculixx/veinminerClient/network/NetworkManager.kt
+++ veinminer-client/src/main/kotlin/de/miraculixx/veinminerClient/network/NetworkManager.kt
@@ -3,10 +3,13 @@
 package de.miraculixx.veinminerClient.network
 
 import de.miraculixx.veinminer.config.extensions.toVeinminer
+import de.miraculixx.veinminer.config.network.BlockHighlighting
 import de.miraculixx.veinminer.config.network.JoinInformation
 import de.miraculixx.veinminer.config.network.KeyPress
 import de.miraculixx.veinminer.config.network.RequestBlockVein
+import de.miraculixx.veinminer.config.network.ServerConfiguration
 import de.miraculixx.veinminer.config.pattern.Pattern
+import de.miraculixx.veinminer.networking.FabricNetworking
 import de.miraculixx.veinminerClient.VeinminerClient
 import de.miraculixx.veinminerClient.constants.*
 import de.miraculixx.veinminerClient.render.BlockHighlightingRenderer
@@ -33,10 +36,17 @@ object NetworkManager {
     var translucentBlockHighlight = true
         private set
 
-    private val onConfiguration = PACKET_CONFIGURATION.receiveOnClient { packet, context ->
+
+    //
+    // Receiving packets from server
+    //
+    private val onConfiguration = PACKET_CONFIGURATION.receiveOnClient { packet, context -> onConfiguration(packet, context.client) }
+    private val onHighlight = PACKET_HIGHLIGHT.receiveOnClient { packet, context -> onHighlight(packet) }
+
+    fun onConfiguration(packet: ServerConfiguration, client: Minecraft) {
         VeinminerClient.LOGGER.info("Server configuration: $packet")
         if (packet.outdated) {
-            context.client.toastManager.addToast(
+            client.toastManager.addToast(
                 SystemToast(SystemToast.SystemToastId.PERIODIC_NOTIFICATION, Component.literal("Veinminer Outdated"), Component.literal("Please update Veinminer"))
             )
         }
@@ -47,12 +57,12 @@ object NetworkManager {
         translucentBlockHighlight = packet.translucentBlockHighlight
     }
 
-    private val onHighlight = PACKET_HIGHLIGHT.receiveOnClient { packet, context ->
+    fun onHighlight(packet: BlockHighlighting) {
         VeinminerClient.LOGGER.info("Received block highlight: $packet")
         if (!packet.allowed) {
             HUDRenderer.updateTarget("forbidden")
             BlockHighlightingRenderer.setShape(emptyList())
-            return@receiveOnClient
+            return
         }
 
         HUDRenderer.updateTarget(packet.icon)
@@ -63,31 +73,52 @@ object NetworkManager {
         isVeinminerActive = false
     }
 
-    fun requestBlockInfo(position: BlockPos, direction: Direction) {
+
+    //
+    // Sending packets to server
+    //
+    fun sendBlockRequest(position: BlockPos, direction: Direction) {
         VeinminerClient.LOGGER.info("Sending veinmine request: ($position, $direction)")
-        Minecraft.getInstance().connection ?: return notConnected()
-        PACKET_MINE.send(RequestBlockVein(position.toVeinminer(), direction.toVeinminer(), selectedPattern))
+        val instance = Minecraft.getInstance()
+        instance.connection ?: return notConnected()
+
+        val packet = RequestBlockVein(position.toVeinminer(), direction.toVeinminer(), selectedPattern)
+        if (shouldSendInternal()) FabricNetworking.onMine(null, packet, instance.player?.uuid)
+        else PACKET_MINE.send(packet)
     }
 
     fun sendJoin(version: String) {
         VeinminerClient.LOGGER.info("Sending join: ($version)")
 
         // Register incoming packets
-        val con = Minecraft.getInstance().connection ?: return notConnected()
+        val instance = Minecraft.getInstance()
+        val con = instance.connection ?: return notConnected()
         con.send(ServerboundCustomPayloadPacket(
             RegistrationPayload(RegistrationPayload.REGISTER, listOf(PACKET_CONFIGURATION.id, PACKET_HIGHLIGHT.id))
         ))
 
-        PACKET_JOIN.send(JoinInformation(version))
+        if (shouldSendInternal()) FabricNetworking.onJoin(null, JoinInformation(version), instance.player?.uuid)
+        else PACKET_JOIN.send(JoinInformation(version))
     }
 
     fun sendKeyPress(pressed: Boolean) {
         VeinminerClient.LOGGER.info("Sending key press: $pressed")
-        Minecraft.getInstance().connection ?: return notConnected()
-        PACKET_KEY_PRESS.send(KeyPress(pressed))
+        val instance = Minecraft.getInstance()
+        instance.connection ?: return notConnected()
+
+        if (shouldSendInternal()) FabricNetworking.onPress(instance.player?.uuid, KeyPress(pressed))
+        else PACKET_KEY_PRESS.send(KeyPress(pressed))
     }
 
+
+    //
+    // Internal
+    //
     private fun notConnected() {
         VeinminerClient.LOGGER.warn("Can not send packet without server connection!")
+    }
+
+    private fun shouldSendInternal(): Boolean {
+        return VeinminerClient.isSinglePlayer && VeinminerClient.veinminerAvailable
     }
 }
--- veinminer-fabric/src/main/kotlin/de/miraculixx/veinminer/networking/FabricNetworking.kt
+++ veinminer-fabric/src/main/kotlin/de/miraculixx/veinminer/networking/FabricNetworking.kt
@@ -6,74 +6,109 @@ import de.miraculixx.veinminer.VeinMinerEvent
 import de.miraculixx.veinminer.VeinMinerEvent.veinmine
 import de.miraculixx.veinminer.Veinminer
 import de.miraculixx.veinminer.config.ConfigManager
+import de.miraculixx.veinminer.config.data.BlockPosition
 import de.miraculixx.veinminer.config.data.VeinminerSettings
 import de.miraculixx.veinminer.config.extensions.toNMS
 import de.miraculixx.veinminer.config.extensions.toVeinminer
 import de.miraculixx.veinminer.config.network.BlockHighlighting
+import de.miraculixx.veinminer.config.network.JoinInformation
+import de.miraculixx.veinminer.config.network.KeyPress
+import de.miraculixx.veinminer.config.network.RequestBlockVein
 import de.miraculixx.veinminer.config.network.ServerConfiguration
 import de.miraculixx.veinminer.config.utils.debug
 import net.minecraft.server.MinecraftServer
 import net.minecraft.server.level.ServerPlayer
 import net.minecraft.server.network.ServerGamePacketListenerImpl
+import net.silkmc.silk.core.Silk
 import java.util.*
 
 object FabricNetworking {
     val registeredPlayers: MutableMap<UUID, String> = mutableMapOf()
     val readyToVeinmine = mutableSetOf<UUID>()
 
+    //
+    // Receive packets from clients
+    //
+    private val onJoin = PACKET_JOIN.receiveOnServer { packet, context -> onJoin(context.player, packet) }
+    private val onPress = PACKET_KEY_PRESS.receiveOnServer { packet, context -> onPress(context.player.uuid, packet) }
+    private val onMine = PACKET_MINE.receiveOnServer { packet, context -> onMine(context.player, packet) }
+
     // Receive client handshake
-    private val onJoin = PACKET_JOIN.receiveOnServer { packet, context ->
-        val player = context.player
-        val uuid = player.uuid
+    fun onJoin(player: ServerPlayer?, packet: JoinInformation, uuid: UUID? = null) {
+        val uuid = player?.uuid ?: uuid ?: return invalidUserInformation("join")
+        val player = player ?: Silk.server?.playerList?.getPlayer(uuid) ?: return invalidUserInformation("join player")
 
         val settings = ConfigManager.settings
-        if (!settings.client.allow) return@receiveOnServer
+        if (!settings.client.allow) return
 
         // Send information back
         Veinminer.LOGGER.info("${player.scoreboardName} joined with Veinminer version ${packet.veinminerClientVersion}")
         registeredPlayers[uuid] = packet.veinminerClientVersion
-        sendConfiguration(context.player, settings)
+        sendConfiguration(player, settings)
     }
 
-    private val onPress = PACKET_KEY_PRESS.receiveOnServer { packet, context ->
-        val uuid = context.player.uuid
+    // Receive key press (true & false)
+    fun onPress(uuid: UUID?, packet: KeyPress) {
+        if (uuid == null) return invalidUserInformation("key press")
+        if (debug) Veinminer.LOGGER.info("$uuid pressed hotkey (${packet.pressed})")
         if (packet.pressed) readyToVeinmine.add(uuid)
         else readyToVeinmine.remove(uuid)
-        if (debug) Veinminer.LOGGER.info("$uuid pressed hotkey (${packet.pressed})")
     }
 
-    private val onMine = PACKET_MINE.receiveOnServer { packet, context ->
-        val uuid = context.player.uuid
+    // Receive veinmine highlight request
+    fun onMine(player: ServerPlayer?, packet: RequestBlockVein, uuid: UUID? = null) {
+        val uuid = player?.uuid ?: uuid ?: return invalidUserInformation("mine")
+        val player = player ?: Silk.server?.playerList?.getPlayer(uuid) ?: return invalidUserInformation("mine player")
+
         if (debug) Veinminer.LOGGER.info("$uuid requested to veinmine block at ${packet.blockPosition}")
 
         // Send feedback
-        val player = context.player
         val level = player.level()
         val position = packet.blockPosition.toNMS()
         val state = level.getBlockState(position)
 
         val veinminerAction = VeinMinerEvent.allowedToVeinmine(level, player, position, state)
         if (veinminerAction == null) {
-            PACKET_HIGHLIGHT.send(BlockHighlighting(false, "", emptyList()), context.player)
-            return@receiveOnServer
+            sendHighlighting(player, false, "", emptyList())
+            return
         }
 
         veinminerAction.copy(settings = veinminerAction.settings.copy(delay = 0)).veinmine(false)
         val blocks = veinminerAction.processedBlocks.map { it.toVeinminer() }
 
-        PACKET_HIGHLIGHT.send(BlockHighlighting(true, VeinMinerEvent.getPreferredToolIcon(state), blocks), context.player)
+        sendHighlighting(player, true, VeinMinerEvent.getPreferredToolIcon(state), blocks)
     }
 
     // Remove any player data when they disconnect
     fun onDisconnect(packet: ServerGamePacketListenerImpl, server: MinecraftServer) {
         val uuid = packet.player.uuid
-        registeredPlayers.remove(uuid)
+        val removed = registeredPlayers.remove(uuid)
         readyToVeinmine.remove(uuid)
+        if (debug && removed != null) Veinminer.LOGGER.info("Removed ${packet.player.scoreboardName} from registered players")
     }
 
+    //
+    // Send packets to clients
+    //
     fun sendConfiguration(player: ServerPlayer, settings: VeinminerSettings) {
         if (!registeredPlayers.containsKey(player.uuid)) return
         val conf = ServerConfiguration(settings.cooldown, settings.mustSneak, false, settings.client.translucentBlockHighlight)
         PACKET_CONFIGURATION.send(conf, player)
+    }
+
+    fun sendHighlighting(player: ServerPlayer, allowed: Boolean, icon: String, blocks: List<BlockPosition>) {
+        if (!registeredPlayers.containsKey(player.uuid)) return userNotConnected(player.uuid)
+        PACKET_HIGHLIGHT.send(BlockHighlighting(allowed, icon, blocks), player)
+    }
+
+    //
+    // Error logging
+    //
+    private fun invalidUserInformation(type: String) {
+        Veinminer.LOGGER.warn("Not enough information to handle '$type' packet!")
+    }
+
+    private fun userNotConnected(uuid: UUID) {
+        Veinminer.LOGGER.warn("Can not send packet to client $uuid (not connected)")
     }
 }
