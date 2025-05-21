d1af93514586b566479fc7939ce7b5e63608197b
miraculixx <miraculixxyt@gmail.com>
2025-05-21 12:51:18 +0200

    Improve platform detection for non cascading classes #36

--- veinminer-paper/src/main/kotlin/de/miraculixx/veinminer/VeinminerCompatibility.kt
+++ veinminer-paper/src/main/kotlin/de/miraculixx/veinminer/VeinminerCompatibility.kt
@@ -3,7 +3,7 @@ package de.miraculixx.veinminer
 object VeinminerCompatibility {
     private val logger = INSTANCE.logger
 
-    val platform by lazy { checkPlatform() }
+    val platform by lazy { checkPlatform().apply { logger.info("Detected platform: $this") } }
     var runsAsync = false
 
     enum class Platform {
@@ -15,27 +15,25 @@ object VeinminerCompatibility {
     }
 
     private fun checkPlatform(): Platform {
-        var platform = Platform.Folia
-        if (runCatching { Class.forName("io.papermc.paper.threadedregions.RegionizedServer") }.isFailure)
-            platform = Platform.Purpur
-        if (runCatching { Class.forName("org.purpurmc.purpur.event.player.PlayerBookTooLargeEvent") }.isFailure)
-            platform = Platform.Paper
-        if (runCatching { Class.forName("io.papermc.paper.event.player.AbstractChatEvent") }.isFailure)
-            platform = Platform.Spigot
-        if (runCatching { Class.forName("org.spigotmc.CustomTimingsHandler") }.isFailure)
-            platform = Platform.Bukkit
-        if (runCatching { Class.forName("org.bukkit.Bukkit") }.isFailure)
-            throw IllegalStateException("How did we get here?")
+        if (runCatching { Class.forName("io.papermc.paper.threadedregions.RegionizedServer") }.isSuccess)
+            return Platform.Folia
+        if (runCatching { Class.forName("org.purpurmc.purpur.event.player.PlayerBookTooLargeEvent") }.isSuccess)
+            return Platform.Purpur
+        if (runCatching { Class.forName("io.papermc.paper.event.player.AbstractChatEvent") }.isSuccess)
+            return Platform.Paper
+        if (runCatching { Class.forName("org.spigotmc.CustomTimingsHandler") }.isSuccess)
+            return Platform.Spigot
+        if (runCatching { Class.forName("org.bukkit.Bukkit") }.isSuccess)
+            return Platform.Bukkit
 
-        logger.info("Detected platform: $platform")
-        return platform
+        throw IllegalStateException("How did we get here?")
     }
 
     // Returns false if the plugin should be stopped
     fun isCompatible(): Boolean {
         when (platform) {
             Platform.Bukkit, Platform.Spigot -> {
-                logger.severe("Veinminer has been loaded on an installation of Bukkit/Spigot!")
+                logger.severe("Veinminer has been loaded on an installation of $platform!")
                 logger.severe("Veinminer does not support anything other than Paper and derivatives!")
                 logger.severe("Spigot is considered legacy, and you should definitively consider upgrading!")
                 logger.severe("For further information, see https://docs.papermc.io/paper/migration")
