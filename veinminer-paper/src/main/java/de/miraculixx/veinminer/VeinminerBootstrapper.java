7bf21441ec9de36fa8d7c10d1e083003976af1e4
miraculixx <miraculixxyt@gmail.com>
2025-09-04 13:29:07 +0200

    Change default to tag format

--- veinminer-paper/src/main/java/de/miraculixx/veinminer/VeinminerBootstrapper.java
+++ veinminer-paper/src/main/java/de/miraculixx/veinminer/VeinminerBootstrapper.java
@@ -2,14 +2,29 @@ package de.miraculixx.veinminer;
 
 import io.papermc.paper.plugin.bootstrap.BootstrapContext;
 import io.papermc.paper.plugin.bootstrap.PluginBootstrap;
+import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents;
 import org.jetbrains.annotations.NotNull;
+
+import java.net.URI;
+import java.util.Objects;
 
 @SuppressWarnings("UnstableApiUsage")
 public class VeinminerBootstrapper implements PluginBootstrap {
 
     @Override
     public void bootstrap(@NotNull BootstrapContext context) {
-        // Reserved for future use
+
+        context.getLifecycleManager().registerEventHandler(
+                LifecycleEvents.DATAPACK_DISCOVERY.newHandler(event -> {
+                    try {
+                        URI uri = Objects.requireNonNull(getClass().getResource("/veinminer_dp")).toURI();
+                        event.registrar().discoverPack(uri, "provided-c-tags");
+                    } catch (Exception e) {
+                        throw new RuntimeException("Failed to discover c: tags datapack", e);
+                    }
+                })
+        );
+
     }
 
 }
