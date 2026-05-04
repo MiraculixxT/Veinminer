452679ffcedb3eee250022249165d7e2520bd793
Miraculixx <julius.bartelheim@gmail.com>
2026-05-04 15:32:47 +0200

    Adding common minecraft exposure

--- veinminer/common/build.gradle.kts
+++ veinminer/common/build.gradle.kts
@@ -1,3 +1,9 @@
 plugins {
+    `kotlin-script`
+    id("net.fabricmc.fabric-loom")
+}
 
+dependencies {
+    val gameVersion: String by properties
+    minecraft("com.mojang:minecraft:$gameVersion")
 }
