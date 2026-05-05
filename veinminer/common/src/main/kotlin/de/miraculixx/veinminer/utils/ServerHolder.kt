ab738df451c8957a8ab7469895bb4cab1451650c
miraculixx <miraculixxyt@gmail.com>
2026-05-06 01:05:35 +0200

    Move server holder to common (NOT used by Paper)

--- veinminer/common/src/main/kotlin/de/miraculixx/veinminer/utils/ServerHolder.kt
+++ veinminer/common/src/main/kotlin/de/miraculixx/veinminer/utils/ServerHolder.kt
@@ -2,5 +2,9 @@ package de.miraculixx.veinminer.utils
 
 import net.minecraft.server.MinecraftServer
 
+/**
+ * Holds a reference to the MinecraftServer instance after launch.
+ * Unused on Paper platforms.
+ */
 @Volatile
 var mcServer: MinecraftServer? = null
