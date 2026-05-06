d140153ca33da794b170455993db00129dee7c0e
miraculixx <miraculixxyt@gmail.com>
2026-05-06 22:43:51 +0200

    Prepare neo modules

--- veinminer-client/neoforge/build.gradle.kts
+++ veinminer-client/neoforge/build.gradle.kts
@@ -1,3 +1,18 @@
 plugins {
+    `kotlin-script`
+    `neoforge-script`
+    `publish-script`
+}
 
+
+dependencies {
+    compileOnly(project(":core"))
+    compileOnly(project(":veinminer:veinminer-common"))
+    implementation(project(":veinminer:veinminer-neoforge"))
+}
+
+sourceSets {
+    main {
+        resources.srcDirs("$rootDir/commons/")
+    }
 }
--- veinminer/neoforge/build.gradle.kts
+++ veinminer/neoforge/build.gradle.kts
@@ -1,3 +1,20 @@
 plugins {
+    `kotlin-script`
+    `neoforge-script`
+    `publish-script`
+}
 
+
+dependencies {
+    implementation(project(":core"))
+    implementation(project(":veinminer:veinminer-common"))
+
+    jarJar(project(":core"))
+    jarJar(project(":veinminer:veinminer-common"))
+}
+
+sourceSets {
+    main {
+        resources.srcDirs("$rootDir/commons/")
+    }
 }
