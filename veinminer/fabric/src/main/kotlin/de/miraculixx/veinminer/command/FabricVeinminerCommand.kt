899e4e64d2fb82eb0f05f9a863c3b3281b684501
miraculixx <miraculixxyt@gmail.com>
2026-05-19 21:12:34 +0200

    Allow commands in every single player world (no cheats required)

--- veinminer/fabric/src/main/kotlin/de/miraculixx/veinminer/command/FabricVeinminerCommand.kt
+++ veinminer/fabric/src/main/kotlin/de/miraculixx/veinminer/command/FabricVeinminerCommand.kt
@@ -4,18 +4,26 @@ import de.miraculixx.veinminer.ActiveConfig
 import de.miraculixx.veinminer.config.ConfigManager
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback
 import net.minecraft.commands.CommandSourceStack
-import net.minecraft.server.permissions.PermissionLevel
 import me.lucko.fabric.api.permissions.v0.Permissions as FabricPermissions
+import net.minecraft.server.permissions.Permissions as VanillaPermissions
 
 object FabricVeinminerCommand {
     fun register() {
         ActiveConfig.bridge = ConfigManager
         Permissions.install { src, node ->
             val css = src as? CommandSourceStack ?: return@install false
-            FabricPermissions.require(node, PermissionLevel.GAMEMASTERS).test(css)
+            css.isSingleplayerOwner() ||
+                FabricPermissions.check(css, node, false) ||
+                css.permissions().hasPermission(VanillaPermissions.COMMANDS_GAMEMASTER)
         }
         CommandRegistrationCallback.EVENT.register { dispatcher, registry, _ ->
             dispatcher.register(VeinminerCommand.build(registry))
         }
+    }
+
+    private fun CommandSourceStack.isSingleplayerOwner(): Boolean {
+        val server = runCatching { server }.getOrNull() ?: return false
+        val player = player ?: return false
+        return server.isSingleplayerOwner(player.nameAndId())
     }
 }
