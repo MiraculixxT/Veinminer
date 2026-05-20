526e9da9d321507283d6da4320e095b9c3f30ec2
miraculixx <miraculixxyt@gmail.com>
2026-05-20 15:07:52 +0200

    Catch fabric starting with null playerlist

--- veinminer/common/src/main/kotlin/de/miraculixx/veinminer/config/ConfigManager.kt
+++ veinminer/common/src/main/kotlin/de/miraculixx/veinminer/config/ConfigManager.kt
@@ -1,5 +1,6 @@
 package de.miraculixx.veinminer.config
 
+import de.miraculixx.veinminer.ActiveConfig
 import de.miraculixx.veinminer.command.ActiveHost
 import de.miraculixx.veinminer.data.BlockGroup
 import de.miraculixx.veinminer.event.EventState
@@ -23,7 +24,7 @@ object ConfigManager : BaseConfigManager<Identifier>(
 ) {
     override fun onAfterReload() {
         val server = mcServer ?: return
-        val playerList = server.playerList
+        val playerList = server.playerList ?: return ActiveHost.host.logger.warn("No player list available!")
         NetworkRouter.registeredPlayers.keys.forEach { uuid ->
             val player = playerList.getPlayer(uuid) ?: return@forEach
             val conf = ServerConfiguration(
