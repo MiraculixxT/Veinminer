e3b984a571029c3d154b9fe5b6788d71a4c4af3a
miraculixx <miraculixxyt@gmail.com>
2026-06-17 13:54:56 +0200

    Fix translucent rendering

--- veinminer-client/common/src/main/kotlin/de/miraculixx/veinminerClient/render/BlockHighlightingRenderer.kt
+++ veinminer-client/common/src/main/kotlin/de/miraculixx/veinminerClient/render/BlockHighlightingRenderer.kt
@@ -1,11 +1,21 @@
 package de.miraculixx.veinminerClient.render
 
+import com.mojang.blaze3d.pipeline.BlendFunction
+import com.mojang.blaze3d.pipeline.ColorTargetState
+import com.mojang.blaze3d.pipeline.DepthStencilState
+import com.mojang.blaze3d.pipeline.RenderPipeline
+import com.mojang.blaze3d.platform.CompareOp
 import com.mojang.blaze3d.vertex.PoseStack
 import de.miraculixx.veinminer.data.BlockPosition
+import de.miraculixx.veinminerClient.ClientLifecycle
 import de.miraculixx.veinminerClient.KeyBindManager
 import de.miraculixx.veinminerClient.network.NetworkManager
 import net.minecraft.client.renderer.OrderedSubmitNodeCollector
+import net.minecraft.client.renderer.RenderPipelines
+import net.minecraft.client.renderer.rendertype.RenderSetup
+import net.minecraft.client.renderer.rendertype.RenderType
 import net.minecraft.client.renderer.rendertype.RenderTypes
+import net.minecraft.resources.Identifier
 import net.minecraft.util.ARGB
 import net.minecraft.world.phys.Vec3
 import net.minecraft.world.phys.shapes.Shapes
@@ -14,6 +24,22 @@ import net.minecraft.world.phys.shapes.VoxelShape
 
 object BlockHighlightingRenderer {
     private var highlightingShape: VoxelShape = Shapes.empty()
+
+    private val renderHighlightingTranslucent: RenderType by lazy {
+        RenderType.create(
+            "${ClientLifecycle.MOD_ID}:highlight_translucent",
+            RenderSetup.builder(
+                RenderPipelines.register(
+                    RenderPipeline.builder(RenderPipelines.LINES_SNIPPET)
+                        .withLocation(Identifier.fromNamespaceAndPath(ClientLifecycle.MOD_ID, "pipeline/highlight_translucent"))
+                        .withDepthStencilState(DepthStencilState(CompareOp.ALWAYS_PASS, false))
+                        .withCull(false)
+                        .withColorTargetState(ColorTargetState(BlendFunction.TRANSLUCENT))
+                        .build()
+                )
+            ).createRenderSetup()
+        )
+    }
 
     fun render(stack: PoseStack, collector: OrderedSubmitNodeCollector, camPos: Vec3) {
         val targetBlock = KeyBindManager.lastTarget
@@ -25,7 +51,7 @@ object BlockHighlightingRenderer {
 
             collector.submitShapeOutline(stack, highlightingShape, RenderTypes.lines(), ARGB.white(255), 1.0f, false)
             if (NetworkManager.settings.client.translucentBlockHighlight) {
-                collector.submitShapeOutline(stack, highlightingShape, RenderTypes.linesTranslucent(), ARGB.white(50), 1.0f, true)
+                collector.submitShapeOutline(stack, highlightingShape, renderHighlightingTranslucent, ARGB.white(50), 1.0f, true)
             }
         } finally {
             stack.popPose()
